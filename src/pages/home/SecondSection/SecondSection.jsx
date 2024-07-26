f1240206ff30ae224fe4be9679c8f9a0651bfec2
ngckhanh <ngockhanh7643@gmail.com>
2024-07-27 00:43:31 +0700

    update

--- src/pages/home/SecondSection/SecondSection.jsx
+++ src/pages/home/SecondSection/SecondSection.jsx
@@ -236,7 +236,7 @@ const SecondSection = () => {
                     ref={constraintsRef}
                 >
                     <motion.div
-                        className="box-1 absolute top-2/4 sm:left-[35%] md:left-[50%] lg:left-[45%] left-[15%] h-[118.5px] w-[118.5px] flex justify-center items-center text-center"
+                        className="box-1 absolute top-2/4 h-[118.5px] w-[118.5px] flex justify-center items-center text-center"
                         drag
                         dragConstraints={constraintsRef}
                         dragElastic={0.5}
@@ -266,7 +266,7 @@ const SecondSection = () => {
                         }}
                     ></motion.div>
                     <motion.div
-                        className="box-4 absolute top-2/4 sm:right-[35%] md:right-[50%] lg:right-[45%] right-[15%] h-[118.5px] w-[118.5px] flex justify-center items-center"
+                        className="box-4 absolute top-2/4 0%] h-[118.5px] w-[118.5px] flex justify-center items-center"
                         drag
                         dragConstraints={constraintsRef}
                         dragElastic={0.5}
