c36ce3fb1119f6891d2551d36105601b2a337cf7
duynguyen-workspace <duynguyen.workspace@gmail.com>
2024-07-29 18:29:05 +0700

    update scale image

--- src/pages/about/GroupImages/GroupImages.jsx
+++ src/pages/about/GroupImages/GroupImages.jsx
@@ -4,36 +4,68 @@ import { motion, useAnimation } from "framer-motion";
 import "./groupimages.scss";
 import { Col, Row } from "antd";
 
+// const imageFileNames = [
+//     "Chi Jade.png",
+//     "DSC00001.png",
+//     "DSC00003.png",
+//     "DSC00010.png",
+//     "DSC00015.png",
+//     "DSC00021.png",
+//     "DSC00036.png",
+//     "DSC00069.png",
+//     "DSC00080.png",
+//     "DSC00083.png",
+//     "DSC00099.png",
+//     "DSC00111.png",
+//     "DSC00113.png",
+//     "DSC00134.png",
+//     "DSC00142.png",
+//     "DSC00146.png",
+//     "DSC00155.png",
+//     "DSC00159.png",
+//     "DSC00173.png",
+//     "DSC00278.png",
+//     "DSC00293.png",
+//     "DSC00178.png",
+//     "DSC00190.png",
+//     "DSC00197.png",
+//     "DSC00206.png",
+//     "DSC00210.png",
+//     "DSC00216.png",
+//     "DSC00251.png",
+//     "DSC00302.png",
+// ];
+
 const imageFileNames = [
-    "Chi Jade.png",
-    "DSC00001.png",
-    "DSC00003.png",
-    "DSC00010.png",
-    "DSC00015.png",
-    "DSC00021.png",
-    "DSC00036.png",
-    "DSC00069.png",
-    "DSC00080.png",
-    "DSC00083.png",
-    "DSC00099.png",
-    "DSC00111.png",
-    "DSC00113.png",
-    "DSC00134.png",
-    "DSC00142.png",
-    "DSC00146.png",
-    "DSC00155.png",
-    "DSC00159.png",
-    "DSC00173.png",
-    "DSC00278.png",
-    "DSC00293.png",
-    "DSC00178.png",
-    "DSC00190.png",
-    "DSC00197.png",
-    "DSC00206.png",
-    "DSC00210.png",
-    "DSC00216.png",
-    "DSC00251.png",
-    "DSC00302.png",
+    "Chi Jade.jpg",
+    "DSC00001.jpg",
+    "DSC00003.jpg",
+    "DSC00010.jpg",
+    "DSC00015.jpg",
+    "DSC00021.jpg",
+    "DSC00036.jpg",
+    "DSC00069.jpg",
+    "DSC00080.jpg",
+    "DSC00083.jpg",
+    "DSC00099.jpg",
+    "DSC00111.jpg",
+    "DSC00113.jpg",
+    "DSC00134.jpg",
+    "DSC00142.jpg",
+    "DSC00146.jpg",
+    "DSC00155.jpg",
+    "DSC00159.jpg",
+    "DSC00173.jpg",
+    "DSC00278.jpg",
+    "DSC00293.jpg",
+    "DSC00178.jpg",
+    "DSC00190.jpg",
+    "DSC00197.jpg",
+    "DSC00206.jpg",
+    "DSC00210.jpg",
+    "DSC00216.jpg",
+    "DSC00251.jpg",
+    "DSC00302.jpg",
 ];
 
 const images = [...imageFileNames, ...imageFileNames];
@@ -63,7 +95,7 @@ const GroupImages = () => {
         const promises = imageFileNames.map(imageName => {
             return new Promise((resolve, reject) => {
                 const image = new Image();
-                image.src = `./assets/member-pics/${imageName}`;
+                image.src = `./assets/members/${imageName}`;
                 image.onload = resolve;
                 image.onerror = reject;
             });
@@ -96,7 +128,7 @@ const GroupImages = () => {
                                 }`}
                             >
                                 <img
-                                    src={`./assets/member-pics/${url}`}
+                                    src={`./assets/members/${url}`}
                                     alt={`image-${index}`}
                                     className="w-full h-full object-cover"
                                 />
