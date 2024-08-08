19c3dad296d39821dbb1293273a694765969b675
ngckhanh <ngockhanh7643@gmail.com>
2024-08-08 22:31:45 +0700

    update newest

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import FirstSection from './FirstSection';
 import SecondSection from './SecondSection'
 import SpeakerSection from './SpeakerSection/SpeakerSection';
+import DraftSpeakerSection from './DraftSpeaker/DraftSpeakerSection';
 
 const Home = () => {
     return (
         <div className="overflow-x-hidden">
-            {/* <FirstSection/> */}
+            <FirstSection/>
             <SecondSection/>
             <SpeakerSection/>
+            <DraftSpeakerSection/>
 
             
         </div>
