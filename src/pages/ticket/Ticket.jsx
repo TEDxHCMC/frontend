19fd3865b12af20b258cfeb64d37bc10790f6e3a
ngckhanh <ngockhanh7643@gmail.com>
2024-08-14 01:58:42 +0700

    update-not-responsive

--- src/pages/ticket/Ticket.jsx
+++ src/pages/ticket/Ticket.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Tracker from "./Tracker";
 import { useSelector } from "react-redux";
+import TicketUI from "./TicketUI";
+
 // import "./ticket.module.scss"
+//import './ticket.scss';
 
 const Ticket = () => {
     const { currStep, ticketData }  = useSelector((state) => state.ticket)
     
     return (
-        <section className="min-h-screen w-10/12 mx-auto py-10">
+        <section className="bg-[#FFFBF6]">
+            <div className="min-h-screen w-10/12 mx-auto py-10">
             <div className="w-full h-full flex flex-col justify-between">
                 <Tracker currentStep={currStep}/>
-                
-                <Outlet />
+                <TicketUI/>
+                {/*<Outlet /> */} 
+            </div>
             </div>
         </section>
     );
