6699428d2e1c7a32a07e3ec08d2d8657b792fdde
duynguyen-workspace <duynguyen.workspace@gmail.com>
2024-08-24 14:23:39 +0700

    Fix header fetcher

--- src/apis/fetcher.js
+++ src/apis/fetcher.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const fetcher = axios.create({
     baseURL: "http://97.74.81.239:8080/",
-    headers: {},
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Access-Control-Allow-Methods": "*",
+    },
     timeout: 30000
 });
 
