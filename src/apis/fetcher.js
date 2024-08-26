9ab9843a81af7e89d476d2dffdbd3949acb14b41
duynguyen-workspace <duynguyen.workspace@gmail.com>
2024-08-26 18:39:41 +0700

    Change fetcher

--- src/apis/fetcher.js
+++ src/apis/fetcher.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-    baseURL: "tedx-server.tedxhochiminhcity.vn:8080/",
+    baseURL: "https://tedx-server.tedxhochiminhcity.vn/",
     headers: {
         "Content-Type": "application/json",
     },
