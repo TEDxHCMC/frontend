693ae9fd95908466581404085cf3a3a9b01fc63b
ngckhanh <ngockhanh7643@gmail.com>
2024-09-10 11:41:12 +0700

    auto play vid

--- src/pages/about/Video/Video.jsx
+++ src/pages/about/Video/Video.jsx
@@ -8,7 +8,7 @@ const Video = ({ videoId }) => {
                 <iframe
                     width="100%"
                     height="100%"
-                    src={`https://www.youtube.com/embed/${videoId}`}
+                    src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
