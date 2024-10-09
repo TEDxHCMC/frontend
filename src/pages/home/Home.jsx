8b986d59e6f10fccfb4fcfc878e0c14ac7ce0ccc
ngckhanh <ngockhanh7643@gmail.com>
2024-10-09 16:27:28 +0700

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import FirstSection from './FirstSection';
 import SecondSection from './SecondSection'
 import SpeakerSection from './SpeakerSection/SpeakerSection';
+import SponsorSection from './SponsorSection';
 
 const Home = () => {
     return (
         <div className="overflow-x-hidden">
             <FirstSection/>
             <SecondSection/>
+            <SponsorSection/>   
             <SpeakerSection/>       
         </div>
     );
---
+++ src/pages/home/SponsorSection/SponsorSection.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const SponsorSection = () => {
+    return (
+        <div className="flex flex-wrap justify-center items-center bg-[#FFFBF6]
+        h-auto p-6 mb-10">
+            <div className="m-4 sm:m-6 md:m-8">
+                <img src="./assets/external_logo/NextGen.png" alt="NextGen Logo"
+                    className="h-[8vw] sm:h-[6vw] md:h-[4vw] lg:h-[3vw] w-auto" />
+            </div>
+            <div className="m-4 sm:m-6 md:m-8">
+                <img src="./assets/external_logo/Carnival.png" alt="Carnival Logo"
+                    className="h-[8vw] sm:h-[6vw] md:h-[4vw] lg:h-[3vw] w-auto" />
+            </div>
+            <div className="m-4 sm:m-6 md:m-8">
+                <img src="./assets/external_logo/La Artisan.png" alt="La Artisan Logo"
+                    className="h-[6vw] sm:h-[6vw] md:h-[4vw] lg:h-[3vw] w-auto" />
+            </div>
+            <div className="m-4 sm:m-6 md:m-8">
+                <img src="./assets/external_logo/mehem.png" alt="Mehem Logo"
+                    className="h-[8vw] sm:h-[6vw] md:h-[4vw] lg:h-[3vw] w-auto" />
+            </div>
+            {/*<div className="m-4 sm:m-6 md:m-8">
+                <img src="./assets/external_logo/mehem.png" alt="AnNam Logo"
+                    className="h-[8vw] sm:h-[6vw] md:h-[4vw] lg:h-[3vw] w-auto" />
+            </div> */}
+        </div>
+    )
+}
+
+export default SponsorSection
